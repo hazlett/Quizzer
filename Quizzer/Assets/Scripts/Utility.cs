304fc1de583b4a11aa663d5feb0fe350d63f1ec4
hazlett <hazlett.206@gmail.com>
2014-12-16 14:56:44 -0500

    Fixed #define in utitlity

--- Quizzer/Assets/Scripts/Utility.cs
+++ Quizzer/Assets/Scripts/Utility.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class Utility
 {
-#if UNITY_WEBPLAYER
-    internal static float SCREENHEIGHT = 1920f;
-    internal static float SCREENWIDTH = 1080f;
-#elif UNITY_ANDROID
+#if UNITY_ANDROID
     internal static float SCREENHEIGHT = 1020f;
     internal static float SCREENWIDTH = 768f;
+#else
+    internal static float SCREENHEIGHT = 1920f;
+    internal static float SCREENWIDTH = 1080f;
 #endif
 
 }
